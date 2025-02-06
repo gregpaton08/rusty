eb5227a7a45b60dc667ef6973c6f0a6c1fdf592f
Gregory Paton <gregpaton08@gmail.com>
2025-02-05 21:34:33 -0500

    working basic prototype

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,7 @@ use axum::{
 };
 use serde_json::json;
 use std::{fs, net::SocketAddr, sync::Arc};
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 
 struct AppState {
@@ -19,18 +20,24 @@ async fn main() {
         image_dir: "images".to_string(),
     });
 
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/images", get(list_images))
         .nest_service("/timelapse", ServeDir::new(&state.image_dir))
+        .layer(cors)
         .with_state(state);
 
     // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    // println!("Server running at http://{}", addr);
-
-    axum::serve(listener, app)
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+    // println!("Server running at http://{}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn list_images(State(state): State<Arc<AppState>>) -> impl IntoResponse {
@@ -49,6 +56,10 @@ async fn list_images(State(state): State<Arc<AppState>>) -> impl IntoResponse {
             images.sort();
             Json(images).into_response()
         }
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to read directory").into_response(),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to read directory",
+        )
+            .into_response(),
     }
 }
